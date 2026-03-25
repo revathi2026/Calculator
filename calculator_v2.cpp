97c35e6c2bc392dec01ab506a89b59dd46df5cf6
Revathi Murali <murali.25434011@nsitifscs.ac.in>
2026-03-25 23:54:22 +0530

    Add Calculator version 2

---
+++ calculator_v2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    float a, b;
+
+    while(true) {
+        cout << "\n--- Calculator Menu ---\n";
+        cout << "1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        if(choice == 5) break;
+
+        cout << "Enter two numbers: ";
+        cin >> a >> b;
+
+        switch(choice) {
+            case 1: cout << "Result = " << a + b; break;
+            case 2: cout << "Result = " << a - b; break;
+            case 3: cout << "Result = " << a * b; break;
+            case 4:
+                if(b != 0) cout << "Result = " << a / b;
+                else cout << "Error!";
+                break;
+            default:
+                cout << "Invalid choice!";
+        }
+    }
+
+    return 0;
+}
